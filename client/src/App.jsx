f28c5b29794ac703c009d04c5f9743a57f95fb89
Kumar0Amit1 <amitmlkumar5689@gmail.com>
2025-08-01 23:23:00 +0530

    Frontend setup completed for version 1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useContext, useEffect } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Auth from "./pages/Auth.jsx";
+import ForgotPassword from "./pages/ForgotPassword.jsx";
+import ResetPassword from "./pages/ResetPassword.jsx";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+import { Context } from "./main";
+import OtpVerification from "./pages/OtpVerification.jsx";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const { setIsAuthenticated, setUser } = useContext(Context);
+
+  useEffect(() => {
+    const getUser = async () => {
+      await axios
+        .get("http://localhost:3000/api/v1/user/me", { withCredentials: true })
+        .then((res) => {
+          setUser(res.data.user);
+          setIsAuthenticated(true);
+        })
+        .catch((err) => {
+          setUser(null);
+          setIsAuthenticated(false);
+        });
+    };
+    getUser();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route
+            path="/otp-verification/:email/:phone"
+            element={<OtpVerification />}
+          />
+          <Route path="/password/forgot" element={<ForgotPassword />} />
+          <Route path="/password/reset/:token" element={<ResetPassword />} />
+        </Routes>
+        <ToastContainer theme="colored" />
+      </Router>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ client/src/layout/Footer.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "../styles/Footer.css";
+import { Link } from "react-router-dom";
+// import fb from "../assets/fb.png";
+// import yt from "../assets/yt.png";
+import git from "../assets/git.png";
+import linkedin from "../assets/linkedin.png";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="footer-logo">
+          <h2>MERN Authentication</h2>
+          <p>Your ultimate guide to mastering the MERN stack.</p>
+        </div>
+        <div className="footer-social">
+          <h3>Follow Me</h3>
+          <div className="social-icons">
+            <Link
+              to="www.linkedin.com/in/amit-kumar-aaaa67326/"
+              target="_blank"
+              className="social-link"
+            >
+              <img src={linkedin} alt="LinkedIn" />
+            </Link>
+            <Link
+              to="https://github.com/Kumar0Amit"
+              target="_blank"
+              className="social-link"
+            >
+              <img src={git} alt="GitHub" />
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="footer-bottom">
+        <p>&copy; 2024 MERN Authentication. All Rights Reserved.</p>
+        <p>Designed by Amit Kumar</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
